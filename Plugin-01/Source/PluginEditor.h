876623703a5963bb7097728b49ae233eabf0456e
Anton Filatov <anton.filatov@eyeem.com>
2020-04-24 13:48:47 +0200

    kadenze plugin 01 init

---
+++ Plugin-01/Source/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Plugin01AudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    Plugin01AudioProcessorEditor (Plugin01AudioProcessor&);
+    ~Plugin01AudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Plugin01AudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Plugin01AudioProcessorEditor)
+};
